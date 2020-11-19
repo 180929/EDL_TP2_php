0ac0cda2156a9f974090ee57c65f375991292505
180929 <celiane.jennebauffe@student.umons.ac.be>
2020-11-19 15:40:05 +0100

    testUpdateOne cours

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -347,6 +347,12 @@ class ProfCoursTest extends TestCase
         * s’inspirer de test de la modification du prof avec idProf= 10 pour tester la modification du cours dans ayant comme idCours = 9.
         *
         */
+        $cours = new Cours($this->intitule, $this->duree, $this->prof);
+        $val = $cours->updateOne($conn, $idCours);
+        $expected_cours_str = $prof->__toString();
+        $record_cours = Cours::printOne($conn, $idCours);
+        $this->assertEquals($expected_cours_str, $record_cours->__toString(), "Update du prof $idCours ...\n");
+        $this->assertTrue($val, "Update du cours num $idCours ...\n");
        
         
         
